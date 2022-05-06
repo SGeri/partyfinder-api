1f12b62acb1506f34c33ec5d10f7647c2b34a172
SGeri <sarffygeri@gmail.com>
2022-05-06 20:52:44 +0200

    connected the routes to frontend

--- src/api.js
+++ src/api.js
@@ -43,10 +43,72 @@ export function getEvents(callback) {
     });
 }
 
+export function createEvent(data, callback) {
+  axios
+    .post(
+      API_BASEURL + "/api/v1/events/create",
+      { data },
+      {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("auth_token")}`,
+        },
+      }
+    )
+    .then(function (response) {
+      if (!response.data.success) {
+        callback({ success: false, error: response.data.error });
+      } else {
+        callback({ success: true });
+      }
+    })
+    .catch(function (error) {
+      if (error.status === 401) {
+        callback({ success: false, error: "Hiányzó hitelesítési token." });
+        return;
+      }
+
+      callback({
+        success: false,
+        error: error.message || JSON.stringify(error) || error,
+      });
+    });
+}
+
+export function editEvent(data, callback) {
+  axios
+    .post(
+      API_BASEURL + "/api/v1/events/edit",
+      { data },
+      {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("auth_token")}`,
+        },
+      }
+    )
+    .then(function (response) {
+      if (!response.data.success) {
+        callback({ success: false, error: response.data.error });
+      } else {
+        callback({ success: true });
+      }
+    })
+    .catch(function (error) {
+      if (error.status === 401) {
+        callback({ success: false, error: "Hiányzó hitelesítési token." });
+        return;
+      }
+
+      callback({
+        success: false,
+        error: error.message || JSON.stringify(error) || error,
+      });
+    });
+}
+
 export function removeEvent(id, callback) {
   axios
     .post(
-      API_BASEURL + "/api/v1/remove_event",
+      API_BASEURL + "/api/v1/events/remove",
       { id },
       {
         headers: {
