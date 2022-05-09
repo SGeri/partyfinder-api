1a2b414c58d56bff5c5d4492318dd8e086e0f9e3
SGeri <sarffygeri@gmail.com>
2022-05-09 20:32:00 +0200

    removed mock data

--- pages/api/v1/get_events.js
+++ pages/api/v1/get_events.js
@@ -12,29 +12,3 @@ export default async function handler(req, res) {
     return;
   }
 }
-
-/* mockdata:
-events: [
-      {
-        id: 1,
-        name: "Offline Party 2021",
-        location: "Storyclub",
-        date: "08.19 19:00",
-        description: "Az Offline Party sorozat 1. része.",
-      },
-      {
-        id: 2,
-        name: "Offline Party 2022",
-        location: "Remix",
-        date: "08.20 20:00",
-        description: "Az Offline Party sorozat 2. része.",
-      },
-      {
-        id: 3,
-        name: "Offline Party 2023",
-        location: "Morrisons 2",
-        date: "08.21 21:00",
-        description: "Az Offline Party sorozat 3. része.",
-      },
-], 
-*/
